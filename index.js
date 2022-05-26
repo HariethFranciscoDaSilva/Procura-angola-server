4471be022e6261d425f82929570bdc05a6c52d84
pedrodls <pedrojoaodev@gmail.com>
2022-05-26 13:50:37 +0100

    sERVER CREATED

---
+++ index.js
@@ -0,0 +1,25 @@
+
+require('dotenv').config()
+
+const express = require('express')
+
+
+const app = express()
+
+const server = require('http').createServer(app)
+
+app.use(express.urlencoded({extended: true}))
+
+app.use(express.json())
+
+app.use('/', (req, res) => {
+
+    res.end('Procura Angola Server API V1 Running...')
+
+})
+
+server.listen(process.env.PORT, () => {
+    console.log(`Server started on http://localhost:${process.env.PORT}`);
+});
+
+
