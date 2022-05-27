8a7b87c2cbe5cd97d5e48e291f116c01b5c54b7a
pedrodls <pedrojoaodev@gmail.com>
2022-05-27 04:18:46 +0100

    NotFOund model created

---
+++ models/notFound.model.js
@@ -0,0 +1,23 @@
+
+
+
+module.exports = ({sequelize, Sequelize}) => {
+
+    const not_found = sequelize.define('not-found', {
+        id: {
+            type: Sequelize.UUID,
+            defaultValue: Sequelize.UUIDV4,
+            primaryKey: true
+        }
+    })
+
+    not_found.associate = (models) => {
+
+        not_found.belongsTo(models.User)
+
+        not_found.belongsTo(models.Information)
+
+    }
+
+    return not_found;
+}
