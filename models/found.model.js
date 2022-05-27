79a13493807205fd2ddd47975ec4ee2e3539c3a6
pedrodls <pedrojoaodev@gmail.com>
2022-05-27 04:17:41 +0100

    Found model created

---
+++ models/found.model.js
@@ -0,0 +1,27 @@
+
+
+
+module.exports = ({sequelize, Sequelize}) => {
+
+    const found = sequelize.define('found', {
+        id: {
+            type: Sequelize.UUID,
+            defaultValue: Sequelize.UUIDV4,
+            primaryKey: true
+        },
+        description: {
+            type: Sequelize.STRING,
+            allowNull: false
+        }
+    })
+
+    found.associate = (models) => {
+
+        found.belongsTo(models.User)
+
+        found.belongsTo(models.Information)
+
+    }
+
+    return found;
+}
