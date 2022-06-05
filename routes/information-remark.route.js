ee17452d4b9eb765e437e3ca1da1cdd9169f1442
pedrodls <pedrojoaodev@gmail.com>
2022-06-05 22:19:21 +0100

    Infor,ation reamrk fixed

--- routes/information-remark.route.js
+++ routes/information-remark.route.js
@@ -3,12 +3,9 @@ const route = require('express').Router()
 const controller = require('../controllers/information-remark.controller')
 
 
-route.post('/',
-    controller.create
-)
+route.post('/', controller.create)
 
-route.get('/length/information/:id',
-    controller.informationIdRemarks
-)
+route.get('/:id', controller.informationIdRemarks)
+
 
 module.exports = route
