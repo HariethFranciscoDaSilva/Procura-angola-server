4c50ffd125cff80d0172971124ca1df292d9a2a3
pedrodls <pedrojoaodev@gmail.com>
2022-06-05 22:29:30 +0100

    Not found controller and route fixed

--- controllers/not-found.controller.js
+++ controllers/not-found.controller.js
@@ -43,3 +43,41 @@ exports.all = async (req, res) => {
     
     }).catch(e => res.json(e))
 } 
+
+exports.one = async (req, res) => {
+
+    const notFound = await NotFound.findOne({
+        order: [["updatedAt", "DESC"]],
+        where: {
+            id: req.params.id
+        },
+        include: [{
+            model: Information,
+            as: 'information',
+            where: {
+                isActive: true
+            },
+            include: [{
+                model: PersonalData,
+                as: 'personalData',
+                include: [{
+                    model: Town,
+                    as: 'town',
+                    include: [{
+                        model: Province,
+                        as: 'province',
+                    }]
+                }]
+            }]
+        }, {
+            model: User,
+            as: 'user'
+        }]
+    }).then(data => {
+
+        data.user.password = ''
+
+        res.json(data)
+    
+    }).catch(e => res.json(e))
+} 
--- routes/not-found.route.js
+++ routes/not-found.route.js
@@ -7,5 +7,7 @@ const controller = require('../controllers/not-found.controller')
 
 route.get('/', controller.all)
 
+route.get('/:id', controller.one)
+
 
 module.exports = route
